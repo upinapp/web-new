982dd9f16397bc6afd9dcebc47472186fbb7ab4d
Vladimir Fedin <vovcyan@MacBook-Pro-3.local>
2018-06-03 15:05:51 -0700

    Fixed production build

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import JssProvider from 'react-jss/lib/JssProvider';
+import { MuiThemeProvider } from 'material-ui/styles';
+import { createGenerateClassName } from '@material-ui/core/styles';
+
 import DashboardPage from './pages/DashboardPage/DashboardPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import MyAppsPage from './pages/MyAppsPage/MyAppsPage';
-import { MuiThemeProvider } from 'material-ui/styles';
 import { Switch, Route } from 'react-router-dom';
 import { CustomTheme } from './configs';
+
+const generateClassName = createGenerateClassName({
+  dangerouslyUseGlobalCSS: true,
+  productionPrefix: 'a',
+});
 
 class App extends React.PureComponent {
   render() {
     return (
       <div>
-        <MuiThemeProvider theme={CustomTheme}>
-          <Switch>
-            <Route path="/auth/" component={LoginPage}/>
-            <Route path="/dashboard" component={DashboardPage}/>
-            <Route path="/apps" component={MyAppsPage}/>
-          </Switch>
-        </MuiThemeProvider>
+        <JssProvider generateClassName={generateClassName}>
+          <MuiThemeProvider theme={CustomTheme}>
+            <Switch>
+              <Route path="/auth/" component={LoginPage}/>
+              <Route path="/dashboard" component={DashboardPage}/>
+              <Route path="/apps" component={MyAppsPage}/>
+            </Switch>
+          </MuiThemeProvider>
+        </JssProvider>
       </div>
     );
   }
